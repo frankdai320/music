499d819a8b50654267c24640a61bfdd391f83417
Frank Dai <frankdai320@gmail.com>
2018-02-24 14:58:12 -0800

    mor estuff

--- main/views.py
+++ main/views.py
@@ -7,6 +7,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from .models import Music
 import re
 import requests
+import json
 
 def index(request):
     return getm(request, 1)
@@ -35,7 +36,7 @@ def add(request):
             else:
                 m = Music(link=id, date_added=timezone.now(), owner=request.POST['name'])
                 m.save()
-                return HttpResponse(request.POST['url'] + " sucessfully added " + "<a href=/" + str(m.id) +">here</a>", content_type="text/plain")
+                return HttpResponse(json.dumps({'id': m.pk, 'url': request.POST['url']}), content_type="application/json")
 
     else:
         return render(request, 'add.html')
@@ -48,3 +49,7 @@ def api(request, musicid):
         return HttpResponse(entry.link, content_type='text/plain')
     except ObjectDoesNotExist:
         return HttpResponse('', content_type='text/plain', status=400)
+
+@csrf_exempt
+def num(request):
+    return HttpResponse(Music.objects.count(), content_type='text/plain')

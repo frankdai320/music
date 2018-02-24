2020971461abd5189c9b4c19d0bcf0db9edeff35
Frank Dai <frankdai320@gmail.com>
2018-02-24 15:00:52 -0800

    debug

--- main/models.py
+++ main/models.py
@@ -5,6 +5,6 @@ class Music(models.Model):
     date_added = models.DateTimeField('date added')
     owner = models.CharField(max_length=200)
     def __str__(self):
-        return self.link + " " + str(self.date_added) + " by " + self.owner
+        return self.link + " " + str(self.date_added) + " by " + self.owner + " id " + self.pk
 
 

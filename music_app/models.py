7f5bece0ad1a1a8ba11d8467865de5ac386ee383
jarhill0 <jarhill0@users.noreply.github.com>
2018-04-15 23:16:29 -0700

    Whitespace changes

--- music_app/models.py
+++ music_app/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
+
 class Music(models.Model):
     link = models.CharField(max_length=200)
     date_added = models.DateTimeField('date added')
     added_by = models.CharField(max_length=200)
+
     def __str__(self):
         return self.link + " " + str(self.date_added) + " by " + self.added_by + " id " + str(self.pk)
-
-

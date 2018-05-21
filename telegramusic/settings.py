fded819fdb0dd99c98ca58cd77b4da0adc29ad71
Frank Dai <frankdai320@gmail.com>
2018-05-21 01:54:24 -0700

    remove forced https

--- telegramusic/settings.py
+++ telegramusic/settings.py
@@ -109,7 +109,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-SECURE_SSL_REDIRECT= True
+#SECURE_SSL_REDIRECT= True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
